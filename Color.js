283c0fb3056693c03d8f0d8f11b25592572cf04b
nbriz <nickbriz@gmail.com>
2020-08-07 19:19:41 -0500

    first push

---
+++ Color.js
@@ -0,0 +1,246 @@
+/*
+    Color
+    -----------
+    by Nick Briz <nickbriz@gmail.com>
+    GNU GPLv3 - https://www.gnu.org/licenses/gpl-3.0.txt
+    2019
+
+    -----------
+       info
+    -----------
+
+    A vanilla JS class with common Color maths
+
+    -----------
+       usage
+    -----------
+
+    // hsv && hsl methods that start w/an underscore expect/return values like:
+    // { h: 0-1, s: 0-1, v: 0-1 }
+    // where as those that don't expect/return values like:
+    // { h: 0-360, s: 0-100, v: 0-100 }
+
+    Color.alpha2hex(a)
+    Color.hex2alpha(hex)
+
+    Color.hex2rgb(hex)
+    Color.hex2hsl(hex)
+    Color._hex2hsl(hex)
+    Color.hex2hsv(hex)
+    Color._hex2hsv(hex)
+
+    Color.rgb2hex(r, g, b)
+    Color.rgb2hsv(r, g, b)
+    Color._rgb2hsv(r, g, b)
+    Color.rgb2hsl(r, g, b)
+    Color._rgb2hsl(r, g, b)
+
+    Color.hsv2hex(h, s, v)
+    Color._hsv2hex(h, s, v)
+    Color.hsv2rgb(h, s, v)
+    Color._hsv2rgb(h, s, v)
+    Color.hsv2hsl(h, s, v)
+    Color._hsv2hsl(h, s, v)
+
+    Color.hsl2hex(h, s, l)
+    Color._hsl2hex(h, s, l)
+    Color.hsl2rgb(h, s, l)
+    Color._hsl2rgb(h, s, l)
+    Color.hsl2hsv(h, s, l)
+    Color._hsl2hsv(h, s, l)
+*/
+class Color {
+  static alpha2hex (a) {
+    const n = a * 255
+    return n.toString(16)
+  }
+
+  static hex2alpha (hex) {
+    return parseInt(hex, 16) / 255
+  }
+
+  static hex2rgb (hex) {
+    const shorthandRegex = /^#?([a-f\d])([a-f\d])([a-f\d])$/i
+    hex = hex.replace(shorthandRegex, function (m, r, g, b) {
+      return r + r + g + g + b + b
+    })
+
+    const res = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex)
+    return res ? {
+      r: parseInt(res[1], 16),
+      g: parseInt(res[2], 16),
+      b: parseInt(res[3], 16)
+    } : null
+  }
+
+  static hex2hsl (hex) {
+    const c = this.hex2rgb(hex)
+    return this.rgb2hsl(c.r, c.g, c.b)
+  }
+
+  static _hex2hsl (hex) {
+    const c = this.hex2rgb(hex)
+    return this._rgb2hsl(c.r, c.g, c.b)
+  }
+
+  static hex2hsv (hex) {
+    const c = this.hex2rgb(hex)
+    return this.rgb2hsv(c.r, c.g, c.b)
+  }
+
+  static _hex2hsv (hex) {
+    const c = this.hex2rgb(hex)
+    return this._rgb2hsv(c.r, c.g, c.b)
+  }
+
+  static rgb2hsv (r, g, b) {
+    const c = this._rgb2hsv(r, g, b)
+    return {
+      h: Math.round(c.h * 360),
+      s: Math.round(c.s * 100),
+      v: Math.round(c.v * 100)
+    }
+  }
+
+  static rgb2hsl (r, g, b) {
+    const c = this._rgb2hsv(r, g, b)
+    const o = this._hsv2hsl(c.h, c.s, c.v)
+    return {
+      h: Math.round(o.h * 360),
+      s: Math.round(o.s * 100),
+      l: Math.round(o.l * 100)
+    }
+  }
+
+  static rgb2hex (r, g, b) {
+    return '#' + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1)
+  }
+
+  static hsv2hex (h, s, v) {
+    const c = this.hsv2rgb(h, s, v)
+    return this.rgb2hex(c.r, c.g, c.b)
+  }
+
+  static hsv2hsl (h, s, v) {
+    h /= 360
+    s /= 100
+    v /= 100
+    const c = this._hsv2hsl(h, s, v)
+    return {
+      h: Math.round(c.h * 360),
+      s: Math.round(c.s * 100),
+      l: Math.round(c.l * 100)
+    }
+  }
+
+  static hsv2rgb (h, s, v) {
+    h /= 360
+    s /= 100
+    v /= 100
+    return this._hsv2rgb(h, s, v)
+  }
+
+  static hsl2hex (h, s, l) {
+    const c = this.hsl2rgb(h, s, l)
+    return this.rgb2hex(c.r, c.g, c.b)
+  }
+
+  static hsl2rgb (h, s, l) {
+    h /= 360
+    s /= 100
+    l /= 100
+    const c = this._hsl2hsv(h, s, l)
+    return this._hsv2rgb(c.h, c.s, c.v)
+  }
+
+  static hsl2hsv (h, s, l) {
+    h /= 360
+    s /= 100
+    l /= 100
+    const c = this._hsl2hsv(h, s, l)
+    return {
+      h: Math.round(c.h * 360),
+      s: Math.round(c.s * 100),
+      v: Math.round(c.v * 100)
+    }
+  }
+
+  static _hsv2hsl (h, s, v) {
+    s = s * v
+    let l = (2 - s) * v
+    s /= (l <= 1) ? l : 2 - l
+    l /= 2
+    return { h, s, l }
+  }
+
+  static _hsl2hsv (h, s, l) {
+    l *= 2
+    s *= (l <= 1) ? l : 2 - 1
+    const v = (1 + s) / 2
+    s = (2 * s) / (l + s)
+    return { h, s, v }
+  }
+
+  static _hsl2rgb (h, s, l) {
+    const c = this._hsl2hsv(h, s, l)
+    return this._hsv2rgb(c.h, c.s, c.v)
+  }
+
+  static _hsl2hex (h, s, l) {
+    const c = this._hsl2rgb(h, s, l)
+    return this.rgb2hex(c.r, c.g, c.b)
+  }
+
+  static _rgb2hsv (r, g, b) {
+    const max = Math.max(r, g, b)
+    const min = Math.min(r, g, b)
+    const dif = max - min
+
+    let h
+    const s = max === 0 ? 0 : dif / max
+    const v = max / 255
+
+    switch (max) {
+      case min: h = 0; break
+      case r: h = (g - b) + dif * (g < b ? 6 : 0); h /= 6 * dif; break
+      case g: h = (b - r) + dif * 2; h /= 6 * dif; break
+      case b: h = (r - g) + dif * 4; h /= 6 * dif; break
+    }
+
+    return { h, s, v }
+  }
+
+  static _rgb2hsl (r, g, b) {
+    const c = this._rgb2hsv(r, g, b)
+    return this._hsv2hsl(c.h, c.s, c.v)
+  }
+
+  static _hsv2hex (h, s, v) {
+    const c = this._hsv2rgb(h, s, v)
+    return this.rgb2hex(c.r, c.g, c.b)
+  }
+
+  static _hsv2rgb (h, s, v) {
+    let r, g, b
+    const i = Math.floor(h * 6)
+    const f = h * 6 - i
+    const p = v * (1 - s)
+    const q = v * (1 - f * s)
+    const t = v * (1 - (1 - f) * s)
+    switch (i % 6) {
+      case 0: r = v; g = t; b = p; break
+      case 1: r = q; g = v; b = p; break
+      case 2: r = p; g = v; b = t; break
+      case 3: r = p; g = q; b = v; break
+      case 4: r = t; g = p; b = v; break
+      case 5: r = v; g = p; b = q; break
+    }
+    return {
+      r: Math.round(r * 255),
+      g: Math.round(g * 255),
+      b: Math.round(b * 255)
+    }
+  }
+}
+
+if (typeof module !== 'undefined') module.exports = Color
--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # Color
-A vanilla JS class with common Color maths
+A vanilla JS class with common Color maths. The methods that handle `hsv` and `hsl` values take/return `h` values between 0-360 and `s`, `v`, `l` values between 0-100. To work with floats, 0-1, add an underscore before the method name, for example `Color._hsv2hex(h, s, v)` takes float values as arguments, or `Color._rgb2hsl(r, g, b)` returns float values, or `Color._hsl2hsv(h, s, l)` both takes and returns values in floats.
+
+```js
+Color.alpha2hex(a)
+Color.hex2alpha(hex)
+
+Color.hex2rgb(hex)
+Color.hex2hsl(hex)
+Color.hex2hsv(hex)
+
+Color.rgb2hex(r, g, b)
+Color.rgb2hsv(r, g, b)
+Color.rgb2hsl(r, g, b)
+
+Color.hsv2hex(h, s, v)
+Color.hsv2rgb(h, s, v)
+Color.hsv2hsl(h, s, v)
+
+Color.hsl2hex(h, s, l)
+Color.hsl2rgb(h, s, l)
+Color.hsl2hsv(h, s, l)
+```
